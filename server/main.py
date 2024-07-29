b5c4b1a2fbad3e2142398e493904582ad6d0fd23
numq <numq.dev@gmail.com>
2024-07-29 05:16:57 +0300

    Provide refactoring

--- server/main.py
+++ server/main.py
@@ -1,8 +1,8 @@
 import os
 
-import waitress
+from waitress import serve
 
 from service import service
 
 if __name__ == '__main__':
-    waitress.serve(service, port=os.environ.get('AUGMENTATION_SERVICE_PORT') or "5000")
+    serve(service, url_scheme='http', host='127.0.0.1', port=os.environ.get('AUGMENTATION_SERVICE_PORT'))
